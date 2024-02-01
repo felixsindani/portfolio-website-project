0b0db5297e2e4d82fe592e1b96aa7e46985c7e93
felixsindani <felixsindani72@gmail.com>
2024-02-01 22:31:25 +0300

    fixing route

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/home")
+def home():
 	return render_template("home.html")
 
 if __name__ == "__main__":
